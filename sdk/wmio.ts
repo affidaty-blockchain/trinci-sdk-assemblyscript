35b0fefd4d827542cfcc2f032607378ca298ae59
Luca Vignali <l.vignali@affidaty.io>
2022-03-11 10:01:23 +0100

    asset and data handler with generic parameter

--- sdk/wmio.ts
+++ sdk/wmio.ts
@@ -1,6 +1,7 @@
 import { default as Utils } from './utils';
 import { default as MsgPack } from './msgpack';
 import { default as Types } from './types';
+import { default as HostFunctions } from './hostfunctions';
 /**
  * Return a TCombinedPtr that contains success and other data in bytearray
  *
@@ -42,4 +43,66 @@ function Null():Types.TCombinedPtr { return MsgPack.appOutputEncode(true, [192])
  * @return {*}  {boolean}
  */
 function Some(data:number[]):boolean { return data.length > 0;  } // null in messagepack
-export { Ok, Error, False, Null, True, Some };
+/**
+ * wrapper of HostFunction.storeData
+ * storeData in owner account using generics, this function take care about MessagePack conversion
+ *
+ * @template T
+ * @param {string} key
+ * @param {T} data
+ * @return {*}  {void}
+ */
+function storeData<T>(key: string, data: T): void {
+    const byteData = MsgPack.serialize<T>(data);
+    return HostFunctions.storeAsset(key,byteData);
+}
+
+/**
+ * wrapper of HostFunction.loadData
+ * loadData from owner account using generics, this function take care about MessagePack conversion
+ * @template T
+ * @param {string} key
+ * @return {*}  {T}
+ */
+function loadData<T>(key: string):T {
+    const bytesResult = HostFunctions.loadData(key);
+    return MsgPack.deserialize<T>(bytesResult);
+}
+/**
+ * wrapper of HostFunction.removeData
+ *
+ * @param {string} key
+ * @return {*}  {void}
+ */
+function removeData(key: string): void {
+    return HostFunctions.removeData(key);
+}
+
+/**
+ * wrapper of HostFunction.storeAsset
+ * storeAsset in any accountID handle by owner using generics, this function take care about MessagePack conversion
+ * @template T
+ * @param {string} accountId
+ * @param {T} value
+ * @return {*}  {void}
+ */
+function storeAsset<T>(accountId: string, value: T): void {
+    const byteData = MsgPack.serialize<T>(value);
+    return HostFunctions.storeAsset(accountId,byteData);    
+}
+
+/**
+ * wrapper of HostFunction.loadAsset
+ * loadAsset from any accountID handle by owner using generics, this function take care about MessagePack conversion
+ * @template T
+ * @param {string} accountId
+ * @return {*}  {T}
+ */
+function loadAsset<T>(accountId: string): T {
+    const bytesResult = HostFunctions.loadAsset(accountId);
+    return MsgPack.deserialize<T>(bytesResult);
+}
+
+
+
+export { Ok, Error, False, Null, True, Some , storeData, loadData , removeData , loadAsset , storeAsset };
