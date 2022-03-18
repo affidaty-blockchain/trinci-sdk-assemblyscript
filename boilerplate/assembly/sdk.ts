c021ea287bedfc2c80bade0e7baf57703fa7bae0
a.vitsiuk <a.vitsiuk@affidaty.io>
2022-03-18 10:57:41 +0100

    sdk imports update

--- boilerplate/assembly/sdk.ts
+++ boilerplate/assembly/sdk.ts
@@ -1,4 +1,4 @@
-import * as sdk from '../node_modules/@affidaty/trinci-sdk-as';
+import * as sdk from '../node_modules/@affidaty/trinci-sdk-as/sdk/sdk';
 
-export * from '../node_modules/@affidaty/trinci-sdk-as';
+export * from '../node_modules/@affidaty/trinci-sdk-as/sdk/sdk';
 export default sdk;
