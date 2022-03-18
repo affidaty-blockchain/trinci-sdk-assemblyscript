f3e48fa2a5d78cc487e11b58a9a346bc69640c44
Luca Vignali <l.vignali@affidaty.io>
2022-03-18 15:09:02 +0100

    added method not found excheption

--- ts_jest/errors.ts
+++ ts_jest/errors.ts
@@ -4,3 +4,4 @@ export const WRONG_ACCOUNT_CONTRACT = 'This account already has a different smar
 export const ACCOUNT_NOT_BOUND = 'This account is not bound to any smart contract.';
 export const ACCOUNT_ALREADY_BINDED = 'This account is already binded';
 export const MODULE_NOT_FOUND = 'Could not find required wasm module.';
+export const METHOD_NOT_FOUND = 'method not found';
