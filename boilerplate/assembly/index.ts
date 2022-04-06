18f89f3bcaf76e791d3d4c0d2619109c4dac04a8
Luca Vignali <l.vignali@affidaty.io>
2022-04-06 18:35:35 +0200

    new boilerplate & example.spec.ts

--- boilerplate/assembly/index.ts
+++ boilerplate/assembly/index.ts
@@ -1,103 +1,114 @@
-import sdk from './sdk';
-    const methodsMap = new Map<string, (ctx: sdk.Types.AppContext, args: u8[])=>sdk.Types.TCombinedPtr>();
-    //####################### Native required functions ###############################################################
-    // exposing heap.alloc function for host to pass data
-    // to this module
-    export function alloc(size: i32): i32 {
-        return heap.alloc(size) as i32;
+import sdk from '../node_modules/@affidaty/trinci-sdk-as';
+import { AccountAssetU64 , OwnerDB } from "../node_modules/@affidaty/trinci-sdk-as/sdk/account";
+const methodsMap = new Map<string, (ctx: sdk.Types.AppContext, args: u8[])=>sdk.Types.WasmResult>();
+//####################### Native required functions ###############################################################
+// exposing heap.alloc function for host to pass data to this module
+export function alloc(size: i32): i32 {
+    return heap.alloc(size) as i32;
+}
+export function run(ctxAddress: i32, ctxSize: i32, argsAddress: i32, argsSize: i32): sdk.Types.WasmResult {
+    // decoding context and getting args from memory
+    let ctxU8Arr: u8[] = sdk.MemUtils.u8ArrayFromMem(ctxAddress, ctxSize);
+    let ctx = sdk.MsgPack.ctxDecode(ctxU8Arr);
+    let argsU8: u8[] = sdk.MemUtils.u8ArrayFromMem(argsAddress, argsSize);
+    
+    // ERROR IF METHOD NOT REGISTERED
+    if (!methodsMap.has(ctx.method)) {
+        let success = false;
+        let resultBytes = sdk.Utils.stringtoU8Array('method not found');
+        return sdk.MsgPack.appOutputEncode(success, resultBytes);
     }
-    export function run(ctxAddress: i32, ctxSize: i32, argsAddress: i32, argsSize: i32): sdk.Types.WasmResult {
-        // decoding context and getting args from memory
-        let ctxU8Arr: u8[] = sdk.MemUtils.u8ArrayFromMem(ctxAddress, ctxSize);
-        let ctx = sdk.MsgPack.ctxDecode(ctxU8Arr);
-        let argsU8: u8[] = sdk.MemUtils.u8ArrayFromMem(argsAddress, argsSize);
-        
-        // ERROR IF METHOD NOT REGISTERED
-        if (!methodsMap.has(ctx.method)) {
-            let success = false;
-            let resultBytes = sdk.Utils.stringtoU8Array('method not found');
-            return sdk.MsgPack.appOutputEncode(success, resultBytes);
-        }
-        // CALL METHOD ASSOCIATED TO STRING
-        return methodsMap.get(ctx.method)(ctx, argsU8);
-    }
-    export function is_callable(methodAddress: i32, methodSize: i32): u8 {
-        // decoding method from memory
-        const calledMethod:string = sdk.MsgPack.deserializeInternalType<string>(sdk.MemUtils.u8ArrayFromMem(methodAddress, methodSize));
-        sdk.HostFunctions.log("Request method:" + calledMethod);
-        // ERROR IF METHOD NOT REGISTERED
-        return methodsMap.has(calledMethod) ? 1 : 0;
-    }
-    //####################### private internal Functions ###############################################################
+    // CALL METHOD ASSOCIATED TO STRING
+    return methodsMap.get(ctx.method)(ctx, argsU8);
+}
+export function is_callable(methodAddress: i32, methodSize: i32): u8 {
+    // decoding method from memory
+    const calledMethod:string = sdk.MsgPack.deserializeInternalType<string>(sdk.MemUtils.u8ArrayFromMem(methodAddress, methodSize));
+    // ERROR IF METHOD NOT REGISTERED
+    return methodsMap.has(calledMethod) ? 1 : 0;
+}
+//####################### private internal Functions ###############################################################
     
 methodsMap.set('test_method', testMethod);
-function testMethod(ctx: sdk.Types.AppContext, argsU8: u8[]): sdk.Types.TCombinedPtr {
+function testMethod(_ctx: sdk.Types.AppContext, argsU8: u8[]): sdk.Types.WasmResult {
     // HostFunctions contains all the available methods to interact with the blockchain
     // outside of protected WASM environment.
     // Use IntelliSense to explore other methods.
     // This one is for logs. To see those logs launch node with at least debug log level
     sdk.HostFunctions.log('testMethod()');
-
-    // resultBytes can be an arbitrary u8[] so you can output data in your own format
-    let success = true;
-    let resultBytes = sdk.Utils.stringtoU8Array('Test method OK');
-    return sdk.MsgPack.appOutputEncode(success, resultBytes);
+    return sdk.Return.True(); // return true in MessagePack
 }
 
 // Use this decorator if you want to use serialize/deserialize generics
 // Some restrictions:
-// Class ctor must be callable without arguments. E.g.:"let c = new MyClass();"
+// Class constructor must be callable without arguments. E.g.:"let c = new MyClass();"
 // supported types: bool, u8, u16, u32, u64, i8, i16, i32, i64, f32, f64, string, ArrayBuffer and arrays of those types.
 // Nested managed classes aren't supported yet.
 // Multidimensional arrays not yet supported.
 // Maps not yet supported
 @msgpackable
-class KeyData {
-    key: string = '';
-    data: ArrayBuffer = new ArrayBuffer(0);
+class Config {
+    owner: string = '';
+    max_mintable : u64 = 0;
+    total_minted : u64 = 0;
+}
+
+methodsMap.set('init', init);
+function init(_ctx: sdk.Types.AppContext, argsU8: u8[]): sdk.Types.WasmResult {
+    // To see those logs launch node with at least "debug" log level
+    sdk.HostFunctions.log('init()');
+    let config = sdk.MsgPack.deserialize<Config>(argsU8);  // you must specify the <Type> for help deserialize function to create output
+
+    if(!OwnerDB.has("init")) { // check if account is already initialized to prevent multiple initializations, has function check if key "init" is present in OwnerDB
+        sdk.HostFunctions.log(`owner: [${config.owner}].`);
+        sdk.HostFunctions.log(`max_mintable: [${config.max_mintable}].`);
+        OwnerDB.set<bool>("init",true); // native types must be stored using set<Type>
+        OwnerDB.setObject("config",config); // you can store any @msgpackable Object with a string key
+        return sdk.Return.True();
+    }
+    return sdk.Return.Error("Account is already initializated!"); // error message must be a string
+}
+methodsMap.set('get_config', get_config);
+function get_config(_ctx: sdk.Types.AppContext, _argsU8: u8[]): sdk.Types.TCombinedPtr {
+    // To see those logs launch node with at least debug log level
+    sdk.HostFunctions.log('get_config()');
+    let config = OwnerDB.getObject<Config>("config"); // you must specify the <Type> for help deserialize funtion to create output
+    sdk.HostFunctions.log(`owner: [${config.owner}].`);
+    sdk.HostFunctions.log(`max_mintable: [${config.max_mintable}].`);
+    return sdk.Return.SuccessAsObject(config); // this row convert "config" in MessagePack and return as WasmResult
 }
 
 @msgpackable
-class Key {
-    key: string = '';
+class TransferArgs {
+    to:string ="";
+    units:u64 =0;
 }
-methodsMap.set('store_data', storeData);
-function storeData(ctx: sdk.Types.AppContext, argsU8: u8[]): sdk.Types.TCombinedPtr {
-    // To see those logs launch node with at least "debug" log level
-    sdk.HostFunctions.log('storeData()');
-    let args = sdk.MsgPack.deserialize<KeyData>(argsU8);
-    sdk.HostFunctions.log(`key: [${args.key}].`);
-    sdk.HostFunctions.log(`data: [${sdk.Utils.arrayBufferToU8Array(args.data).toString()}].`);
-    sdk.HostFunctions.storeData(args.key, sdk.Utils.arrayBufferToU8Array(args.data));
-
-    // resultBytes can be an arbitrary u8[] so you can output data in your own format
-    let success = true;
-    let resultBytes = sdk.Utils.stringtoU8Array('Data stored');
-    return sdk.MsgPack.appOutputEncode(success, resultBytes);
-}
-methodsMap.set('load_data', loadData);
-function loadData(ctx: sdk.Types.AppContext, argsU8: u8[]): sdk.Types.TCombinedPtr {
-    // To see those logs launch node with at least debug log level
-    sdk.HostFunctions.log('loadData()');
-    let args = sdk.MsgPack.deserialize<Key>(argsU8);
-    sdk.HostFunctions.log(`key: [${args.key}].`);
-    let data = sdk.HostFunctions.loadData(args.key);
-    sdk.HostFunctions.log(`data: [${data.toString()}].`);
-
-    // resultBytes can be an arbitrary u8[] so you can output data in your own format
-    let success = true;
-    return sdk.MsgPack.appOutputEncode(success, data);
+methodsMap.set('mint', mint);
+function mint(ctx: sdk.Types.AppContext, argsU8: u8[]): sdk.Types.WasmResult {
+    sdk.HostFunctions.log('Mint Coin');
+    let config = OwnerDB.getObject<Config>("config"); // retrieve config from db
+    if(ctx.origin == config.owner) { // only config.owner can mint, ctx.origin is who signed the transaction
+        let args = sdk.MsgPack.deserialize<TransferArgs>(argsU8); // convert generic u8[] that contains bytes of MessagePack of config structure
+        const toAccount = new AccountAssetU64(args.to); // use AccountAssetU64 class to wrap usesful method around a generic asset in u64
+        toAccount.add(args.units); // add take care about previous balance and add token to actual balance
+        return sdk.Return.Success<u64>(toAccount.balance()); // in Success<Type>  you must specify native type such as u64,string, bool, ecc...
+    }
+    return sdk.Return.Error("No right to mint!"); // error message must be a string
 }
 
-methodsMap.set('remove_data', removeData);
-function removeData(ctx: sdk.Types.AppContext, argsU8: u8[]): sdk.Types.TCombinedPtr {
-    // To see those logs launch node with at least debug log level
-    sdk.HostFunctions.log('loadData()');
-    let args = sdk.MsgPack.deserialize<Key>(argsU8);
-    sdk.HostFunctions.log(`key: [${args.key}].`);
-    sdk.HostFunctions.removeData(args.key);
-
-    let success = true;
-    let resultBytes = sdk.Utils.stringtoU8Array('Data removed');
-    return sdk.MsgPack.appOutputEncode(success, resultBytes);
+methodsMap.set('transfer', transfer);
+function transfer(ctx: sdk.Types.AppContext, argsU8: u8[]): sdk.Types.WasmResult {
+    let args = sdk.MsgPack.deserialize<TransferArgs>(argsU8); // convert generic u8[] that contains bytes of MessagePack of config structure
+    // retrive payer account from ctx, you can use ctx.caller or ctx.origin,
+    const fromAccount = new AccountAssetU64(ctx.caller);
+    // but if you use ctx.caller you open yourself to scenarios in which other smart contracts 
+    //can make token shifts, while if you force ctx.origin, only the one who is able to sign transactions will be able to move tokens
+    if(fromAccount.sub(args.units)) { // .sub return true if the balance is enough to make payment
+        const toAccount = new AccountAssetU64(args.to);
+        toAccount.add(args.units);
+        return sdk.Return.Success<u64>(fromAccount.balance());
+    } else {
+        //Insufficent funds
+        return sdk.Return.Error(`insufficent funds in ${ctx.caller}, balance:${fromAccount.balance()} transfer:${args.units}`);    
+    }
 }
--- boilerplate/test/example.spec.ts
+++ boilerplate/test/example.spec.ts
@@ -1,33 +1,134 @@
 import path from 'path';
 import { TrinciNode, TX } from '@affidaty/trinci-sdk-as/ts_jest';
-
-describe('desc1', () => {
-    it('it1', async () => {
+// remember to run npm run as build to compile smart contrct
+describe('Test all smart contract functionalities', () => {
+    it('test method', async () => {
         // getting smart contract full path
         const wasmFilePath = path.resolve(__dirname, '../build/release.wasm');
 
         // creating new TrinciNode
-        const n = new TrinciNode();
+        const node = new TrinciNode();
 
         // registering smart contract inside node and getting it's reference hash
-        const scRefHash = await n.registerContract(wasmFilePath);
+        const scRefHash = await node.registerContract(wasmFilePath);
 
         // creating new mocked transaction
         const tx = new TX()
-        .network('net_str')
-        .target('target_acc')
-        .contract(scRefHash)
-        .args('a b c d e')
+        .target('#TargetAccount')
+        .contract(scRefHash) // in this moment #TargetAccount doesn't exist, you must specify the hash to create account and bind it with specific smart contract
         .method('test_method')
-        .signer('signer_acc');
+        .signer('MyDeployAccount'); // in this environement you can use any AccountId as string, in production you must use a private key and Node derives accountId from them
 
         // running smart contract by passing it our mocked transaction
-        let runResult = await n.runTx(tx);
+        let txResult = await node.runTx(tx);
 
         // checking execution result
-        if (runResult.isError) {
-            console.log(`Error: ${runResult.errorMessage}`)
+        if (txResult.isError) {
+            console.log(`Error: ${txResult.errorMessage}`)
         }
-        expect(Buffer.from(runResult.result).toString()).toEqual('Test method OK');
+        expect(txResult.resultDecoded).toEqual(true);
+        node.db.printData("#TargetAccount");
+        
+    });
+    it('init method', async () => {
+        // getting smart contract full path
+        const wasmFilePath = path.resolve(__dirname, '../build/release.wasm');
+
+        // creating new TrinciNode
+        const node = new TrinciNode();
+
+        // registering smart contract inside node and getting its reference hash
+        const scRefHash = await node.registerContract(wasmFilePath);
+
+        // creating new mocked transaction
+        const tx = new TX()
+        .target('#TargetAccount')
+        .contract(scRefHash) // in this moment #TargetAccount doesn't exist, you must specify the hash to create account and bind it with specific smart contract
+        .method('init')
+        .args({owner : "MyOwner", max_mintable: 100000000,total_minted : 0}) // config object compatible as defined in smart contract as class Config {....}
+        .signer('MyDeployAccount'); // in this environment you can use any AccountId as string, in production you must use a private key and Node derives accountId from them
+
+        // running smart contract by passing it our mocked transaction
+        let txResult = await node.runTx(tx);
+
+        // checking execution result
+        if (txResult.isError) {
+            console.log(`Error: ${txResult.errorMessage}`);
+        }
+        expect(txResult.resultDecoded).toEqual(true); // check blockchain response
+        expect(node.db.getDataFromOwnerDB("#TargetAccount","init") == true); // check if data was stored in DB
+        node.db.printData("#TargetAccount");  // use this tool to print in console the table of KVStore about any account such as TargetAccount
+    });
+    it('mint method', async () => {
+        // getting smart contract full path
+        const wasmFilePath = path.resolve(__dirname, '../build/release.wasm');
+
+        // creating new TrinciNode
+        const node = new TrinciNode();
+
+        // registering smart contract inside node and getting its reference hash
+        const scRefHash = await node.registerContract(wasmFilePath);
+
+        // creating new mocked transaction
+        const tx = new TX()
+        .target('#TargetAccount')
+        .contract(scRefHash) // in this moment #TargetAccount doesn't exist, you must specify the hash to create account and bind it with specific smart contract
+        .method('init')
+        .args({owner : "MyOwner", max_mintable: 100000000,total_minted : 0}) // config object compatible as defined in smart contract as class Config {....}
+        .signer('MyDeployAccount'); // in this environment you can use any AccountId as string, in production you must use a private key and Node derives accountId from them
+
+        // running smart contract by passing it our mocked transaction
+        await node.runTx(tx); // we don't take care about result
+        const mintedToken = 1000;
+        const mintTx = new TX()
+        .target('#TargetAccount')
+        //.contract(scRefHash) you can ignore contract in this scenareo, because the first TX bind forever #TargetAccount with smart contract release.wasm
+        .method('mint')
+        .args({
+            to:"AnotherAccount",
+            units:mintedToken
+        }) // TransferArgs object compatible as defined in smart contract as class TransferArgs {....}
+        .signer('MyOwner'); // the Mint rule allow only MyOwner account to mint, as defined in previous tx {owner: "MyOwner" ....}
+        const txResult = await node.runTx(mintTx);
+        // checking execution result
+        if (txResult.isError) {
+            console.log(`Error: ${txResult.errorMessage}`);
+        }
+        expect(txResult.resultDecoded).toEqual(mintedToken); // check blockchain response, mint return the balance of minted account 
+        expect(node.db.balance("#TargetAccount","AnotherAccount") == mintedToken); // check if balance of AnotherAccount is equal to minted tokens
+        node.db.printAssets();  // use this tool to print in console the table of KVStore about any account such as TargetAccount
+    });
+    it('transfer method', async () => {
+        // getting smart contract full path
+        const wasmFilePath = path.resolve(__dirname, '../build/release.wasm');
+
+        // creating new TrinciNode
+        const node = new TrinciNode();
+
+        // registering smart contract inside node and getting its reference hash
+        const scRefHash = await node.registerContract(wasmFilePath);
+
+        // creating new mocked transactions
+        await node.runTx(new TX().target('#TargetAccount').contract(scRefHash).method('init').args({owner : "MyOwner", max_mintable: 100000000,total_minted : 0}).signer('MyDeployAccount')); // init
+        await node.runTx(new TX().target('#TargetAccount').method('mint').args({to:"AnotherAccount",units:1000}).signer('MyOwner')); //  mint
+        
+        const transferTx = new TX()
+        .target('#TargetAccount') // specify token account
+        //.contract(scRefHash) you can ignore contract in this scenareo, because the first TX bind forever #TargetAccount with smart contract release.wasm
+        .method('transfer')
+        .args({
+            to:"DestAccount",
+            units:100 // transfer 100 tokens
+        }) // TransferArgs object compatible as defined in smart contract as class TransferArgs {....}
+        .signer('AnotherAccount'); // the Transfer rule allow only Account's Owner to move token from his account, so the signature must be created by AnotherAccount
+        const txResult = await node.runTx(transferTx);
+        // checking execution result
+        if (txResult.isError) {
+            console.log(`Error: ${txResult.errorMessage}`);
+        }
+        expect(txResult.resultDecoded).toEqual((1000 - 100)); // check blockchain response, transfer return the balance of owner account 
+        expect(node.db.balance("#TargetAccount","AnotherAccount") == (1000 - 100)); // check if balance of AnotherAccount is equal to minted tokens - transfer tokens
+        expect(node.db.balance("#TargetAccount","DestAccount") ==  100); // check if balance of AnotherAccount is equal to minted tokens - transfer tokens
+        node.db.printAssets();  // use this tool to print in console the table of KVStore about any account such as TargetAccount
     });
 });
