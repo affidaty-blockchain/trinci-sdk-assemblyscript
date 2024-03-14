fefa5352cf89d5d473f9a1232d2ac53ed26526b6
a.vitsiuk <a.vitsiuk@affidaty.io>
2024-03-14 16:59:06 +0100

    added user confirmation and -y option to the initialization script

--- init.js
+++ init.js
@@ -2,6 +2,20 @@
 
 import fs from 'fs';
 import path, { dirname } from 'path';
+import { default as ReadLine , Interface as ReadLineInterface}  from 'readline';
+import Yargs from 'yargs/yargs';
+
+const argv = Yargs(Yargs.hideBin(process.argv))
+.version('1.0.0')
+.locale('en')
+.option('yes', {
+    alias: 'y',
+    type: 'boolean',
+    demandOption: false,
+    description: 'If set, no overwrite confirmation is needed',
+})
+.help()
+.argv;
 
 /**
  * recursively copies the content of source into target (or merges if target exists)
@@ -56,8 +70,61 @@ function copyOrMergeDirsRecursiveSync(source, target, overwriteFiles = false) {
     })
 }
 
-copyOrMergeDirsRecursiveSync(
-    path.resolve(import.meta.dirname, './boilerplate'),
-    path.resolve('./'),
-    true,
-);
+
+/**
+ * @param {string} question
+ * @param {boolean} defaultResponse
+ * @param {ReadLineInterface | null} customRLInterface
+ * @returns {Promise<boolean>}
+ */
+function askForUserConfirmation(question, defaultResponse, customRLInterface) {
+    return new Promise((resolve) => {
+        let _rlInterface = customRLInterface || ReadLine.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+        const promptStr = `${question}(${defaultResponse ? 'YES/no' : 'yes/NO'}): `;
+        _rlInterface.question(promptStr, async(answer) => {
+            if (!answer || !answer.length) {
+                if (!customRLInterface) _rlInterface.close();
+                return resolve(defaultResponse);
+            }
+            if (['y', 'yes'].indexOf(answer.trim().toLowerCase()) >= 0) {
+                if (!customRLInterface) _rlInterface.close();
+                return resolve(true);
+            } else if (['n', 'no'].indexOf(answer.trim().toLowerCase()) >= 0) {
+                if (!customRLInterface) _rlInterface.close();
+                return resolve(false);
+            }
+            console.log(`Please enter a valid answer: y/yes/n/no`);
+            const recursiveAnswer = await askForUserConfirmation(question, defaultResponse, _rlInterface);
+            if (!customRLInterface) _rlInterface.close();
+            return resolve(recursiveAnswer);
+        })
+    });
+}
+
+async function main() {
+    const sourcePath = path.resolve(import.meta.dirname, './boilerplate');
+    const targetPath = path.resolve('./');
+    if (!argv.yes) {
+        console.log();
+        console.log(`This operation will merge folder\n"${sourcePath}"\ninto current working folder\n"${targetPath}"\noverwriting any existing file.`);
+        console.log();
+        if (!(await askForUserConfirmation('Are you sure?'))) {
+            console.log('Cancelled by user');
+            console.log();
+            return;
+        }
+        console.log();
+    }
+
+    copyOrMergeDirsRecursiveSync(
+        sourcePath,
+        targetPath,
+        true,
+    );
+    return;
+}
+
+main();
